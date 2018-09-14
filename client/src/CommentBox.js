1bfd27839ae9d721e3f78c6314c5a3732e6be9cf
Carlos Cruz Ramos <josp551@gmail.com>
2018-09-14 16:25:42 -0500

    CommentBox now makes API calls to add to and get comments from db

--- client/src/CommentBox.js
+++ client/src/CommentBox.js
@@ -28,6 +28,35 @@ class CommentBox extends Component {
         this.pollInterval = null;
     }
 
+    loadCommentsFromServer = () => {
+        fetch('/api/comments/')
+            .then(data => data.json())
+            .then((res) => {
+                if (!res.success) this.setState({error: res.error});
+                else this.setState({data: res.data});
+            });
+    };
+
+    onChangeText = (e) => {
+        const newState = {...this.state};
+        newState[e.target.name] = e.target.value;
+        this.setState(newState);
+    };
+
+    submitComment = (e) => {
+        e.preventDefault();
+        const {author, comment} = this.state;
+        if (!author || !comment) return;
+        fetch('/api/comments', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({author, comment})
+        }).then(res => res.json()).then((res) => {
+            if (!res.success) this.setState({error: res.error.message || res.error});
+            else this.setState({author: '', text: '', error: null});
+        });
+    };
+
     render() {
         return (
             <div className="container">
@@ -36,7 +65,10 @@ class CommentBox extends Component {
                     <CommentList data={this.state.data} />
                 </div>
                 <div className="form">
-                    <CommentForm author={this.state.author} text={this.state.text}/>
+                    <CommentForm
+                        author={this.state.author} text={this.state.text}
+                        handleChangeText={this.onChangeText} submitComment={this.submitComment}
+                    />
                 </div>
                 {this.state.error && <p>{this.state.error}</p>}
             </div>
